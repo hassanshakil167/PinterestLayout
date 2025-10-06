93c1fa4962fae235b34a7b5943daf95e8a511a90
Hassan Shakil <hassan.shakil@techliance.com>
2025-10-06 15:21:25 +0500

    Fix actor isolation in PinterestLayoutAttributes for Swift 6

--- Sources/PinterestLayout/PinterestLayoutAttributes.swift
+++ Sources/PinterestLayout/PinterestLayoutAttributes.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 
-
-/**
- CollectionViewLayoutAttributes.
- */
 public class PinterestLayoutAttributes: UICollectionViewLayoutAttributes {
-    /**
-     Image height to be set to contstraint in collection view cell.
-     */
-    @preconcurrency @MainActor public var imageHeight: CGFloat = 0
-    
+    public var imageHeight: CGFloat = 0
+
     override public func copy(with zone: NSZone? = nil) -> Any {
         let copy = super.copy(with: zone) as! PinterestLayoutAttributes
         copy.imageHeight = imageHeight
         return copy
     }
-    
+
     override public func isEqual(_ object: Any?) -> Bool {
-        if let attributes = object as? PinterestLayoutAttributes {
-            if attributes.imageHeight == imageHeight {
-                return super.isEqual(object)
-            }
+        guard let attributes = object as? PinterestLayoutAttributes else {
+            return false
+        }
+
+        // Safely read actor-isolated state (imageHeight) from a nonisolated context
+        let thisHeight = MainActor.assumeIsolated { self.imageHeight }
+        let otherHeight = MainActor.assumeIsolated { attributes.imageHeight }
+
+        if thisHeight == otherHeight {
+            return super.isEqual(object)
         }
         return false
     }
 }
+
